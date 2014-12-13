5c5de35a4f41f6dbc6cc486af1df9eab2ded7d5b
Sebastian Osorio <svzosorio@boolinc.co>
2014-12-13 13:38:20 -0500

    Results

--- app/js/config/services.js
+++ app/js/config/services.js
@@ -46,6 +46,9 @@ angular.module('config.services', [])
       if(response.status == 500){
           message = "Ha ocurrido un error en el servidor";
         }
+        else if(response.status == 0){
+          message = "No hay conección a internet";
+        }
         else if(errors[response.status]){
           if(errors[response.status][response.data.error.message]){
             message = errors[response.status][response.data.error.message];
